8e9b6f63a9114d976c786f077599554473560da0
ccaioffelipe <caiofrs0612@gmail.com>
2025-05-07 23:55:07 -0300

    TelaInicial
    Fiz essa tela, não está completa.
    Adicionei um método para centralizar Label no Panel

---
+++ FatecPoo/TelaInicial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FatecPoo
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            string Nprisioneiro = "100";
+            lblNprisioneiro.Text = $" {Nprisioneiro}";
+            string RelatorioPen = "50";
+            lblRelatorioPen.Text = $"{RelatorioPen}";
+            string CelaDis = "40";
+            lblCelaDis.Text = $"{CelaDis}";
+
+        }
+
+        private void CentralizarLabel(Label lbl, Panel pnl)
+        {
+            lbl.Left = (pnl.Width - lbl.Width) / 2;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+            CentralizarLabel(lblNprisioneiro, pnlPrisioneiro);
+            CentralizarLabel(lblCelaDis, panel1);
+            CentralizarLabel(lblRelatorioPen, panel2);
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
