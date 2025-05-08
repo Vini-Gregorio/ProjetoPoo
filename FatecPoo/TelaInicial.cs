1997d46ce354efc0a3eda22498665274ac7a248e
ccaioffelipe <caiofrs0612@gmail.com>
2025-05-08 09:37:12 -0300

    Update TelaInicial.cs

--- FatecPoo/TelaInicial.cs
+++ FatecPoo/TelaInicial.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FatecPoo
 {
-    public partial class Form2 : Form
+    public partial class TelaInicial : Form
     {
-        public Form2()
+        public TelaInicial()
         {
             InitializeComponent();
 
