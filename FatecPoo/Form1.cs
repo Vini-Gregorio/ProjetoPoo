30cf63c7dfdccc890413663e262613356cd7263c
Vinícius Gregório <1testee102030@gmail.com>
2025-05-08 09:26:18 -0300

    Minhas alterações

--- FatecPoo/Form1.cs
+++ FatecPoo/Form1.cs
@@ -2,6 +2,7 @@ namespace FatecPoo
 {
     public partial class Form1 : Form
     {
+        List<Pessoa> listadedetentos = new List<Pessoa>();
         public Form1()
         {
             InitializeComponent();
@@ -10,6 +11,92 @@ namespace FatecPoo
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelNascimento_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LimparCampos()
+        {
+            txtNome.Text = "";
+            txtNascimento.Text = "";
+            txtEmail.Text = "";
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+            Pessoa p = new Pessoa();
+            int idade;
+            if (!int.TryParse(txtNascimento.Text, out idade))
+            {
+                MessageBox.Show("Idade inválida. Digite apenas números.");
+                return;
+            }
+            p.Nome = txtNome.Text;
+            p.Nascimento = idade;
+            p.Email = txtEmail.Text;
+
+            listadedetentos.Add(p);
+
+            MessageBox.Show(" Detento adicionado ");
+            LimparCampos();
+
+        }
+
+        private void Listar_Click(object sender, EventArgs e)
+        {
+            listDetentos.Items.Clear();
+
+            foreach (var p in listadedetentos)
+            {
+                listDetentos.Items.Add($"Nome: {p.Nome}, Idade: {p.Nascimento}, Email: {p.Email}");
+            }
+            LimparCampos();
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            int index = listDetentos.SelectedIndex;
+            if (index >= 0)
+            {
+                listadedetentos.RemoveAt(index);
+                MessageBox.Show("Pessoa removida!");
+                LimparCampos();
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma pessoa da lista.");
+            }
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            int index = listDetentos.SelectedIndex;
+            int idade;
+           
+            if (index >= 0)
+            {
+                if (!int.TryParse(txtNascimento.Text, out idade))
+                {
+                    MessageBox.Show("Idade inválida. Digite apenas números.");
+                    return;
+                }
+                listadedetentos[index].Nome = txtNome.Text;
+                listadedetentos[index].Email = txtEmail.Text;
+                listadedetentos[index].Nascimento = idade;
+
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma pessoa da lista.");
+            }
+            LimparCampos();
         }
     }
 }
